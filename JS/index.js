8c2ee1c19b4923e679c77b6bf72e9d026e54f1d9
Lenis Cervantes <leniserik@gmail.com>
2022-11-22 15:34:31 +0100

    Projects appear in homepage

--- JS/index.js
+++ JS/index.js
@@ -1,28 +1,64 @@
 
-fetch("https://raw.githubusercontent.com/ironhack-jc/mid-term-api/main/projects")
-    .then((response) => {
-        return response.json();
-    })
-    .then((data) => {
-        console.log("Parsed response: ", data);
-    })
-    .catch((err) => console.log(err));
+const pr1Img = document.querySelector('#card1-img')
+const pr1Name = document.querySelector('#card1-name')
+const pr1Description = document.querySelector('#card1-description')
+
+const pr2Img = document.querySelector('#card2-img')
+const pr2Name = document.querySelector('#card2-name')
+const pr2Description = document.querySelector('#card2-description')
+
+const pr3Img = document.querySelector('#card3-img')
+const pr3Name = document.querySelector('#card3-name')
+const pr3Description = document.querySelector('#card3-description')
+
+const card1Data = { image: pr1Img, name: pr1Name, description: pr1Description }
+const card2Data = { image: pr2Img, name: pr2Name, description: pr2Description }
+const card3Data = { image: pr3Img, name: pr3Name, description: pr3Description }
+
+let pr1 = {}
+let pr2 = {}
+let pr3 = {}
+let pr4 = {}
 
 
-    
-    // let btnProject = document.querySelectorAll
-    //     .addEventListener('click', () => { })
-    
-    // fetch("https://api.spacexdata.com/v4/launches")
-    // .then((response) => response.json())
-    // .then((data) => {
-    //   data.forEach((launchObj) => {
-    //     const patchImage = launchObj.links.patch.small;
-    //     const imgElement = document.createElement("img");
-    
-    //     imgElement.setAttribute("src", patchImage);
-    //     imgElement.setAttribute("width", 200);
-    //     document.body.appendChild(imgElement);
-    //   });
-    // })
-    // .catch((err) => console.log(err));
+// const assignCard1 = ({ image, name, description }) => {
+//     pr1Img.setAttribute("src", image)
+//     pr1Name.innerHTML = name
+//     pr1Description.innerHTML = description
+// }
+// const assignCard2 = ({ image, name, description }) => {
+//     pr2Img.setAttribute('src', image)
+//     pr2Name.innerHTML = name
+//     pr2Description.innerHTML = description
+// }
+// const assignCard3 = ({ image, name, description }) => {
+//     pr3Img.setAttribute('src', image)
+//     pr3Name.innerHTML = name
+//     pr3Description.innerHTML = description
+// }
+
+const assignCard = (card, { image, name, description }) => {
+    card.image.setAttribute('src', image)
+    card.name.innerHTML = name
+    card.description.innerHTML = description
+}
+
+window.onload = () => {
+    console.log('hey, everything loaded')
+    fetch("https://raw.githubusercontent.com/ironhack-jc/mid-term-api/main/projects")
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            pr4 = data[0]
+            pr3 = data[1]
+            pr2 = data[2]
+            pr1 = data[3]
+        })
+        .then(() => {
+            assignCard(card1Data, pr1)
+            assignCard(card2Data, pr2)
+            assignCard(card3Data, pr3)
+        })
+        .catch((err) => console.log(err));
+};
