a1c41e6b598d4f01b58879dd5dcfc1ac096df301
leniscervantes <109067378+leniscervantes@users.noreply.github.com>
2022-11-23 09:55:52 +0100

    bit more refactored code (#5)
    * better routing on all navbars
    
    * small css changes on all pages
    
    * code a bit more refactored

--- JS/index.js
+++ JS/index.js
@@ -1,19 +1,19 @@
 
-const pr1Img = document.querySelector('#card1-img')
-const pr1Name = document.querySelector('#card1-name')
-const pr1Description = document.querySelector('#card1-description')
+let pr1Img = document.querySelector('#card1-img')
+let pr1Name = document.querySelector('#card1-name')
+let pr1Description = document.querySelector('#card1-description')
 
-const pr2Img = document.querySelector('#card2-img')
-const pr2Name = document.querySelector('#card2-name')
-const pr2Description = document.querySelector('#card2-description')
+let pr2Img = document.querySelector('#card2-img')
+let pr2Name = document.querySelector('#card2-name')
+let pr2Description = document.querySelector('#card2-description')
 
-const pr3Img = document.querySelector('#card3-img')
-const pr3Name = document.querySelector('#card3-name')
-const pr3Description = document.querySelector('#card3-description')
+let pr3Img = document.querySelector('#card3-img')
+let pr3Name = document.querySelector('#card3-name')
+let pr3Description = document.querySelector('#card3-description')
 
-const card1Data = { image: pr1Img, name: pr1Name, description: pr1Description }
-const card2Data = { image: pr2Img, name: pr2Name, description: pr2Description }
-const card3Data = { image: pr3Img, name: pr3Name, description: pr3Description }
+let card1Data = { image: pr1Img, name: pr1Name, description: pr1Description }
+let card2Data = { image: pr2Img, name: pr2Name, description: pr2Description }
+let card3Data = { image: pr3Img, name: pr3Name, description: pr3Description }
 
 let pr1 = {}
 let pr2 = {}
@@ -36,6 +36,12 @@ let pr4 = {}
 //     pr3Name.innerHTML = name
 //     pr3Description.innerHTML = description
 // }
+const assignPrjs = (data) => {
+    pr4 = data[0];
+    pr3 = data[1];
+    pr2 = data[2];
+    pr1 = data[3];
+}
 
 const assignCard = (card, { image, name, description }) => {
     card.image.setAttribute('src', image)
@@ -50,12 +56,7 @@ window.onload = () => {
             return response.json();
         })
         .then((data) => {
-            pr4 = data[0]
-            pr3 = data[1]
-            pr2 = data[2]
-            pr1 = data[3]
-        })
-        .then(() => {
+            assignPrjs(data)
             assignCard(card1Data, pr1)
             assignCard(card2Data, pr2)
             assignCard(card3Data, pr3)
